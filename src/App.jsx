d8b38582d66ec6ee3f895d1e31050fe85de2d43d
lautarohelp <lautarotoledo391@gmail.com>
2024-01-25 10:53:44 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <header>
+        {/* <img src="" alt="logo" />
+        <li>
+          <a href="">hola</a>
+          <a href="">chau</a>
+          <a href="">hola</a>
+          <ul>
+            <a href=""></a>
+            <a href=""></a>
+            <a href=""></a>
+            <a href=""></a>
+            <a href=""></a>
+          </ul>
+          <a href="">chau</a>
+          <a href="">cahu</a>
+        </li>
+        <nav>
+          <img src="" alt="" />
+          <p></p>
+        </nav> */}
+      </header>
+      <main>
+      </main>
+      <footer></footer>
+    </>
+  )
+}
+
+export default App
