96c9e5e561dc2756e169e108545a1664c01cd417
unknown <lorenzopenalver3@gmail.com>
2024-01-31 13:42:37 -0300

    Arreglo de Bugs

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -1,16 +1,11 @@
 import { FadeIn } from '../Componenets-animation/FadeIn'
 import PropTypes from 'prop-types';
 import './cards.css'
-import { FadeIn } from '../Componenets animation/animation.jsx'
 
 function Card({ img, order, title, text }) {
     if (order === 1) {
         return (
             <>
-<<<<<<< HEAD
-            <FadeIn>
-                <section className='card_Maincontainer opacity'>
-=======
             <FadeIn 
                 directionStart='x'
                 start='20'
@@ -20,7 +15,6 @@ function Card({ img, order, title, text }) {
                 >
                 <section className='card_Maincontainer'>
                 
->>>>>>> 6e8979b77d6b615f9fe3792675bc56a4fb89a125
                     <section className='card_container'>
                         <div className={`cardTitle_container${order}`}><h1> {title}</h1></div>
                         <p>{text}</p>
@@ -29,21 +23,13 @@ function Card({ img, order, title, text }) {
                     <img src={img} />
 
                 </section>
-<<<<<<< HEAD
-                </FadeIn>
-=======
             </FadeIn>
->>>>>>> 6e8979b77d6b615f9fe3792675bc56a4fb89a125
             </>
         )
     }
     if (order === 2){
         return (
             <>
-<<<<<<< HEAD
-            <FadeIn>
-                 <section className='card_Maincontainer2'>
-=======
             <FadeIn
             directionStart='x'
             start='-20'
@@ -52,18 +38,13 @@ function Card({ img, order, title, text }) {
             time='1.5'
             >
                 <section className='card_Maincontainer2'>
->>>>>>> 6e8979b77d6b615f9fe3792675bc56a4fb89a125
                     <img src={img} />
                     <section className={`card_container card_container${order}`}>
                         <div className={`cardTitle_container${order}`}><h1>{title}</h1></div>
                         <p>{text}</p>
                     </section>
                 </section>
-<<<<<<< HEAD
-                </FadeIn>
-=======
             </FadeIn>
->>>>>>> 6e8979b77d6b615f9fe3792675bc56a4fb89a125
             </>
         )
     }
